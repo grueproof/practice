b5f013b3df412905af1efc24350b177be2a5f6d8
Chris Miller <ctmiller@pm.me>
2019-09-24 14:17:11 -0400

    First solution

---
+++ 102/colors.py
@@ -0,0 +1,22 @@
+VALID_COLORS = ['blue', 'yellow', 'red']
+
+
+def print_colors():
+    """In the while loop ask the user to enter a color,
+       lowercase it and store it in a variable. Next check: 
+       - if 'quit' was entered for color, print 'bye' and break. 
+       - if the color is not in VALID_COLORS, print 'Not a valid color' and continue.
+       - otherwise print the color in lower case."""
+    while True:
+        userinput = input("what color? ")
+        if userinput.lower() not in VALID_COLORS:
+            if userinput == "quit":
+                print("bye")
+                break
+            else:
+                print("Not a valid color")
+        else:
+           print(userinput.lower())
+
+if __name__ == "__main__":
+    print_colors()
